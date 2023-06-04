ac088513a4edb81042377a24d3076f137958e848
samahrens <ahrens5@purdue.edu>
2023-06-03 22:51:04 -0700

    got XML from dining court api

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
+import datetime
+import pytz
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_date():
+    current_time = datetime.datetime.utcnow()
+    est = pytz.timezone('US/Eastern')
+    current_time_est = current_time.astimezone(est)
+    current_date = current_time_est.strftime('%Y-%m-%d')
+    return current_date
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def main():
+    current_date = get_date()
+    earhart_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Earhart/{current_date}")
+    ford_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Ford/{current_date}")
+    hillenbrand_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Hillenbrand/{current_date}")
+    wiley_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Wiley/{current_date}")
+    windsor_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Windsor/{current_date}")
+
+    xml_content = windsor_xml.content.decode('utf-8')
+    print(xml_content)
+
+
+if __name__ == "__main__":
+    main()
