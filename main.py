916bbf75c6c67307183d9c371044dff1835a9f54
samahrens <89820822+samahrens@users.noreply.github.com>
2023-06-29 23:35:39 -0700

    added API fetching logic

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 import datetime
 import pytz
+import pyodbc
+import json
+
+connection_string = f""
+table_name = ''
 
 
 def get_date():
@@ -11,16 +16,52 @@ def get_date():
     return current_date
 
 
-def main():
-    current_date = get_date()
-    earhart_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Earhart/{current_date}")
-    ford_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Ford/{current_date}")
-    hillenbrand_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Hillenbrand/{current_date}")
-    wiley_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Wiley/{current_date}")
-    windsor_xml = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/Windsor/{current_date}")
+def format_json(name, date):
+    req_info = requests.get(f"https://api.hfs.purdue.edu/menus/v2/locations/{name}/{date}")
+    json_content = req_info.json()
+    print(json_content)
+    json_dump = json.dumps(json_content)
+    return json_dump
 
-    xml_content = windsor_xml.content.decode('utf-8')
-    print(xml_content)
+
+def main():
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    # Clear all entries from the table
+    delete_all_query = f'DELETE FROM {table_name}'
+    cursor.execute(delete_all_query)
+
+    current_date = get_date()
+
+    dining_courts = ["Earhart", "Ford", "Hillenbrand", "Wiley", "Windsor"]
+
+    for dining_court in dining_courts:
+        json_string = format_json(dining_court, current_date)
+
+        # Load and Insert the JSON data into the table
+        data = json.loads(json_string)
+        meals = data['Meals']
+
+        if len(meals) == 0:
+            continue
+
+        rows_to_insert = [
+            (data['Date'], data['Location'], meal['Name'], item['Name'])
+            for meal in meals
+            for station in meal['Stations']
+            for item in station['Items']
+        ]
+
+        found_item = len(rows_to_insert) > 0
+
+        # Perform batch insert
+        if found_item:  # ensures there is data
+            insert_query = f"INSERT INTO {table_name} (Date, Location, Type, Name) VALUES (?, ?, ?, ?)"
+            cursor.executemany(insert_query, rows_to_insert)
+
+    conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
