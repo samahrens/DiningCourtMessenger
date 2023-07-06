60e624fc63c38d57136017748ef145fd05d9a499
Sam Ahrens <89820822+samahrens@users.noreply.github.com>
2023-07-06 10:43:57 -0700

    added notification message with data

--- notification.py
+++ notification.py
@@ -20,23 +20,29 @@ def send_text():
     email_sender = ""
     email_password = ""
     email_receiver = ''
-    subject = "from sam"
-    body = """
-        
-    """
+    subject = "DiningCourtMessenger"
 
     em = EmailMessage()
     em['From'] = email_sender
     em['To'] = email_receiver
     em['subject'] = subject
-    em.set_content(body)
 
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
 
+        for entry in entries:
+            date = entry[0]
+            dining_court = entry[1]
+            meal = entry[2]
+            food = entry[3]
+            body = f"""
+                    {food} is being served at {dining_court} dining court for {meal} today, {date}!
+                """
+            em.set_content(body)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
+            
 
 def main():
     conn = pyodbc.connect(connection_string)
